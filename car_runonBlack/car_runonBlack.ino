eeb0587491794bc8d06ce2a0ac149c607ccdec4d
chadedas <64877724+chadedas@users.noreply.github.com>
2023-12-07 11:17:18 +0700

    modif

--- car_runonBlack/car_runonBlack.ino
+++ car_runonBlack/car_runonBlack.ino
@@ -8,7 +8,7 @@ int preError = 0;
 int sumError = 0;
 
 const int numSensors = 5;  // จำนวนเซนเซอร์
-const int irSensorPins[numSensors] = {2, 3, 4, 5, 6};  // ขา Digital Input ที่เชื่อมต่อกับเซนเซอร์
+const int irSensorPins[numSensors] = {6, 5, 4, 3, 2};  // ขา Digital Input ที่เชื่อมต่อกับเซนเซอร์
 int irSensorValues[numSensors];  // เก็บค่าที่ได้จากเซนเซอร์
 int error = 0;  // เก็บค่า error
 
@@ -24,45 +24,66 @@ void loop() {
   for (int i = 0; i < numSensors; i++) {
     irSensorValues[i] = digitalRead(irSensorPins[i]);  // อ่านค่า Digital จากเซนเซอร์
   }
-  if (irSensorValues[0] == LOW && irSensorValues[1] == LOW && irSensorValues[2] == LOW && irSensorValues[3] == LOW && irSensorValues[4] == HIGH) {
+  if (irSensorValues[0] == LOW && irSensorValues[1] == LOW && irSensorValues[2] == LOW && irSensorValues[3] == LOW && irSensorValues[4] == LOW) {
     error = 4;
-  } else if (irSensorValues[0] == LOW && irSensorValues[1] == LOW && irSensorValues[2] == LOW && irSensorValues[3] == HIGH && irSensorValues[4] == HIGH) {
+          Serial.print("Error Value: ");
+      Serial.println(error);
+  } else if (irSensorValues[0] == LOW && irSensorValues[1] == LOW && irSensorValues[2] == LOW && irSensorValues[3] == HIGH && irSensorValues[4] == LOW) {
     error = 3;
+          Serial.print("Error Value: ");
+      Serial.println(error);
   } else if (irSensorValues[0] == LOW && irSensorValues[1] == LOW && irSensorValues[2] == LOW && irSensorValues[3] == HIGH && irSensorValues[4] == LOW) {
     error = 2;
+          Serial.print("Error Value: ");
+      Serial.println(error);
   } else if (irSensorValues[0] == LOW && irSensorValues[1] == LOW && irSensorValues[2] == HIGH && irSensorValues[3] == HIGH && irSensorValues[4] == LOW) {
     error = 1;
+          Serial.print("Error Value: ");
+      Serial.println(error);
   } else if (irSensorValues[0] == LOW && irSensorValues[1] == LOW && irSensorValues[2] == HIGH && irSensorValues[3] == LOW && irSensorValues[4] == LOW) {
     error = 0;
+          Serial.print("Error Value: ");
+      Serial.println(error);
   } else if (irSensorValues[0] == LOW && irSensorValues[1] == HIGH && irSensorValues[2] == HIGH && irSensorValues[3] == LOW && irSensorValues[4] == LOW) {
     error = -1;
+          Serial.print("Error Value: ");
+      Serial.println(error);
   } else if (irSensorValues[0] == LOW && irSensorValues[1] == HIGH && irSensorValues[2] == LOW && irSensorValues[3] == LOW && irSensorValues[4] == LOW) {
     error = -2;
+          Serial.print("Error Value: ");
+      Serial.println(error);
   } else if (irSensorValues[0] == HIGH && irSensorValues[1] == HIGH && irSensorValues[2] == LOW && irSensorValues[3] == LOW && irSensorValues[4] == LOW) {
     error = -3;
+          Serial.print("Error Value: ");
+      Serial.println(error);
   } else if (irSensorValues[0] == HIGH && irSensorValues[1] == LOW && irSensorValues[2] == LOW && irSensorValues[3] == LOW && irSensorValues[4] == LOW) {
     error = -4;
-  } else {
-    Serial.print("ERROR!");  // หากไม่ตรงกับเงื่อนไขใด ๆ ให้กำหนด error เป็น 0
+      Serial.print("Error Value: ");
+      Serial.println(error);
   }
 
-  // แสดงค่า error ทาง Serial Monitor
-  Serial.print("Error Value: ");
-  Serial.println(error);
 
-
-  int speedChange = Kp*error + Kd*(error-preError);
+  int speedChange = Kp * error + Kd * (error - preError);
   int MotorLeft = baseSpeed + speedChange;
 
-  int motorSpeed = Kp*error + Kd*(error - preError);
+  int motorSpeed = Kp * error + Kd * (error - preError);
   int leftSpeed = baseSpeed + motorSpeed;
   int rightSpeed = baseSpeed - motorSpeed;
 
-  if(leftSpeed > maxSpeed) leftSpeed = maxSpeed;
-  if(rightSpeed > maxSpeed) rightspeed = maxSpeed;
 
-  if(leftSpeed < -maxSpeed) leftSpeed = -maxSpeed;
-  if(rightSpeed < -maxSpeed) rightspeed = -maxSpeed;
+  if(leftSpeed > maxSpeed) 
+  leftSpeed = maxSpeed;
+  if(rightSpeed > maxSpeed) 
+  rightSpeed = maxSpeed;
+
+  if(leftSpeed < -maxSpeed) 
+  leftSpeed = -maxSpeed;
+  if(rightSpeed < -maxSpeed) 
+  rightSpeed = -maxSpeed;
+
+ //motor(1,leftSpeed);
+ //motor(2,rightSpeed);
+ 
 
   preError = error;
   sumError += error;
