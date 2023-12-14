c3ade9bbde45d6e649888ef699bd94b3cce5b544
chadedas <64877724+chadedas@users.noreply.github.com>
2023-12-14 10:29:45 +0700

    Auto Format

--- car_runonBlack/car_runonBlack.ino
+++ car_runonBlack/car_runonBlack.ino
@@ -6,10 +6,10 @@ int maxSpeed = 255;
 int preError = 0;
 int sumError = 0;
 
-const int numSensors = 5;  // จำนวนเซนเซอร์
-const int irSensorPins[numSensors] = {6, 5, 4, 3, 2};  // ขา Digital Input ที่เชื่อมต่อกับเซนเซอร์
-int irSensorValues[numSensors];  // เก็บค่าที่ได้จากเซนเซอร์
-int error = 0;  // เก็บค่า error
+const int numSensors = 5;                                // จำนวนเซนเซอร์
+const int irSensorPins[numSensors] = { 6, 5, 4, 3, 2 };  // ขา Digital Input ที่เชื่อมต่อกับเซนเซอร์
+int irSensorValues[numSensors];                          // เก็บค่าที่ได้จากเซนเซอร์
+int error = 0;                                           // เก็บค่า error
 
 void setup() {
   Serial.begin(9600);  // เริ่มต้น Serial communication
@@ -24,75 +24,73 @@ void loop() {
   for (int i = 0; i < numSensors; i++) {
     irSensorValues[i] = digitalRead(irSensorPins[i]);  // อ่านค่า Digital จากเซนเซอร์
   }
-    Serial.print("State: ");
+  Serial.print("State: ");
   for (int i = 0; i < numSensors; i++) {
-    if (irSensorValues[i] == HIGH){
-    irSensorValues[i] = 0;
-    Serial.print(irSensorValues[i]);
-    }else{
-    irSensorValues[i] = 1;
-    Serial.print(irSensorValues[i]);
+    if (irSensorValues[i] == HIGH) {
+      irSensorValues[i] = 0;
+      Serial.print(irSensorValues[i]);
+    } else {
+      irSensorValues[i] = 1;
+      Serial.print(irSensorValues[i]);
     }
   }
-     // เพื่อขึ้นบรรทัดใหม่ 
+  // เพื่อขึ้นบรรทัดใหม่
   Serial.println();  // เพื่อขึ้นบรรทัดใหม่
 
   if (irSensorValues[0] == LOW && irSensorValues[1] == LOW && irSensorValues[2] == LOW && irSensorValues[3] == LOW && irSensorValues[4] == HIGH) {
     error = 4;
-    
+
   } else if (irSensorValues[0] == LOW && irSensorValues[1] == LOW && irSensorValues[2] == LOW && irSensorValues[3] == HIGH && irSensorValues[4] == HIGH) {
     error = 3;
-    
+
   } else if (irSensorValues[0] == LOW && irSensorValues[1] == LOW && irSensorValues[2] == LOW && irSensorValues[3] == HIGH && irSensorValues[4] == LOW) {
     error = 2;
-    
+
   } else if (irSensorValues[0] == LOW && irSensorValues[1] == LOW && irSensorValues[2] == HIGH && irSensorValues[3] == HIGH && irSensorValues[4] == LOW) {
     error = 1;
-    
+
   } else if (irSensorValues[0] == LOW && irSensorValues[1] == LOW && irSensorValues[2] == HIGH && irSensorValues[3] == LOW && irSensorValues[4] == LOW) {
     error = 0;
-    
+
   } else if (irSensorValues[0] == LOW && irSensorValues[1] == HIGH && irSensorValues[2] == HIGH && irSensorValues[3] == LOW && irSensorValues[4] == LOW) {
     error = -1;
-    
+
   } else if (irSensorValues[0] == LOW && irSensorValues[1] == HIGH && irSensorValues[2] == LOW && irSensorValues[3] == LOW && irSensorValues[4] == LOW) {
     error = -2;
-    
+
   } else if (irSensorValues[0] == HIGH && irSensorValues[1] == HIGH && irSensorValues[2] == LOW && irSensorValues[3] == LOW && irSensorValues[4] == LOW) {
     error = -3;
-    
+
   } else if (irSensorValues[0] == HIGH && irSensorValues[1] == LOW && irSensorValues[2] == LOW && irSensorValues[3] == LOW && irSensorValues[4] == LOW) {
     error = -4;
-
   }
-  
 
-if (irSensorValues[0] == LOW && irSensorValues[1] == LOW && irSensorValues[2] == LOW && irSensorValues[3] == LOW && irSensorValues[4] == LOW){
+
+  if (irSensorValues[0] == LOW && irSensorValues[1] == LOW && irSensorValues[2] == LOW && irSensorValues[3] == LOW && irSensorValues[4] == LOW) {
     Serial.println("Not found Black Line !!!");
-}else{
-        Serial.print("Error Value: ");
-      Serial.println(error);
-  sumError += error;
-  int speedChange = Kp * error + Kd * (error - preError);
-  int leftSpeed = baseSpeed + speedChange;
-  int rightSpeed = baseSpeed - speedChange;
+  } else {
+    Serial.print("Error Value: ");
+    Serial.println(error);
+    sumError += error;
+    int speedChange = Kp * error + Kd * (error - preError);
+    int leftSpeed = baseSpeed + speedChange;
+    int rightSpeed = baseSpeed - speedChange;
 
-  // จำกัดความเร็วให้อยู่ในช่วงที่กำหนด
-  if (leftSpeed > maxSpeed) leftSpeed = maxSpeed;
-  if (rightSpeed > maxSpeed) rightSpeed = maxSpeed;
-  if (leftSpeed < -maxSpeed) leftSpeed = -maxSpeed;
-  if (rightSpeed < -maxSpeed) rightSpeed = -maxSpeed;
+    // จำกัดความเร็วให้อยู่ในช่วงที่กำหนด
+    if (leftSpeed > maxSpeed) leftSpeed = maxSpeed;
+    if (rightSpeed > maxSpeed) rightSpeed = maxSpeed;
+    if (leftSpeed < -maxSpeed) leftSpeed = -maxSpeed;
+    if (rightSpeed < -maxSpeed) rightSpeed = -maxSpeed;
 
-  // แสดงค่าความเร็วทาง Serial Monitor
-  Serial.print("Left Speed: ");
-  Serial.println(leftSpeed);
-  Serial.print("Right Speed: ");
-  Serial.println(rightSpeed);
-  Serial.print("Sum Error: ");
-  Serial.println(sumError);
-  
-  error = 0;
+    // แสดงค่าความเร็วทาง Serial Monitor
+    Serial.print("Left Speed: ");
+    Serial.println(leftSpeed);
+    Serial.print("Right Speed: ");
+    Serial.println(rightSpeed);
+    Serial.print("Sum Error: ");
+    Serial.println(sumError);
 
-}
+    error = 0;
+  }
   delay(1000);
-} 
+}
