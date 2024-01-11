e883fb77078a3aef12e246a60c6c11d0df401ba5
chadedas <64877724+chadedas@users.noreply.github.com>
2024-01-11 14:15:47 +0700

    MODIF

--- car_runonBlack/car_runonBlack.ino
+++ car_runonBlack/car_runonBlack.ino
@@ -1,18 +1,42 @@
-float june = 1.45124;
-
-
-float Kp = 20*june;
-float Kd = 5*june;
-int baseSpeed = 50;
-int maxSpeed = 255;
-
+int Kp = 25;
+int Kd = 0;
+int Ki = 0;
+int error = 0;
 int preError = 0;
 int sumError = 0;
+
+int N = 5;
+int lineV = 0;
+int groundV = 0;
+int meanV = 500;
+
+int baseSpeed = 100;
+int maxSpeed = 255;
+int rightSpeed,leftSpeed;
+int motorSpeed;
+
+int motorLeft = 11;
+int motorRight = 12;
+
+bool B(int n){  
+  if(n < meanV){ // is black
+      return true;
+  }else{
+      return false;
+  } 
+}
+
+bool W(int n){  
+    if(n >= meanV){ // is white
+      return true;
+  }else{
+      return false;
+  } 
+}
 
 const int numSensors = 5;                                // จำนวนเซนเซอร์
 const int irSensorPins[numSensors] = { 6, 5, 4, 3, 2 };  // ขา Digital Input ที่เชื่อมต่อกับเซนเซอร์
 int irSensorValues[numSensors];                          // เก็บค่าที่ได้จากเซนเซอร์
-int error = 0;                                           // เก็บค่า error
 
 void setup() {
   Serial.begin(9600);  // เริ่มต้น Serial communication
@@ -20,79 +44,63 @@ void setup() {
   for (int i = 0; i < numSensors; i++) {
     pinMode(irSensorPins[i], INPUT);  // กำหนดขา Digital เป็นขานำเข้า
   }
+    pinMode(motorLeft, OUTPUT);
+    pinMode(motorRight, OUTPUT);
 }
 
 void loop() {
-  Serial.println("");
-  for (int i = 0; i < numSensors; i++) {
-    irSensorValues[i] = digitalRead(irSensorPins[i]);  // อ่านค่า Digital จากเซนเซอร์
-  }
-  Serial.print("State: ");
-  for (int i = 0; i < numSensors; i++) {
-    if (irSensorValues[i] == HIGH) {
-      irSensorValues[i] = 0;
-      Serial.print(irSensorValues[i]);
-    } else {
-      irSensorValues[i] = 1;
-      Serial.print(irSensorValues[i]);
+    for(int i=0;i<N;i++){
+        irSensorValues[i] = analogRead(i);
     }
-  }
-  // เพื่อขึ้นบรรทัดใหม่
-  Serial.println();  // เพื่อขึ้นบรรทัดใหม่
-
-  if (irSensorValues[0] == LOW && irSensorValues[1] == LOW && irSensorValues[2] == LOW && irSensorValues[3] == LOW && irSensorValues[4] == HIGH) {
-    error = 4;
-
-  } else if (irSensorValues[0] == LOW && irSensorValues[1] == LOW && irSensorValues[2] == LOW && irSensorValues[3] == HIGH && irSensorValues[4] == HIGH) {
-    error = 3;
-
-  } else if (irSensorValues[0] == LOW && irSensorValues[1] == LOW && irSensorValues[2] == LOW && irSensorValues[3] == HIGH && irSensorValues[4] == LOW) {
-    error = 2;
-
-  } else if (irSensorValues[0] == LOW && irSensorValues[1] == LOW && irSensorValues[2] == HIGH && irSensorValues[3] == HIGH && irSensorValues[4] == LOW) {
-    error = 1;
-
-  } else if (irSensorValues[0] == LOW && irSensorValues[1] == LOW && irSensorValues[2] == HIGH && irSensorValues[3] == LOW && irSensorValues[4] == LOW) {
-    error = 0;
-
-  } else if (irSensorValues[0] == LOW && irSensorValues[1] == HIGH && irSensorValues[2] == HIGH && irSensorValues[3] == LOW && irSensorValues[4] == LOW) {
-    error = -1;
-
-  } else if (irSensorValues[0] == LOW && irSensorValues[1] == HIGH && irSensorValues[2] == LOW && irSensorValues[3] == LOW && irSensorValues[4] == LOW) {
-    error = -2;
-
-  } else if (irSensorValues[0] == HIGH && irSensorValues[1] == HIGH && irSensorValues[2] == LOW && irSensorValues[3] == LOW && irSensorValues[4] == LOW) {
-    error = -3;
-
-  } else if (irSensorValues[0] == HIGH && irSensorValues[1] == LOW && irSensorValues[2] == LOW && irSensorValues[3] == LOW && irSensorValues[4] == LOW) {
-    error = -4;
-  }
-
-
+   
+    if( W(irSensorValues[0]) && W(irSensorValues[1]) && W(irSensorValues[2]) && W(irSensorValues[3]) && B(irSensorValues[4]) ){ 
+        error = 4;
+    }else if( W(irSensorValues[0]) && W(irSensorValues[1]) && W(irSensorValues[2]) && B(irSensorValues[3]) && B(irSensorValues[4]) ){
+      error = 3;
+    }else if( W(irSensorValues[0]) && W(irSensorValues[1]) && W(irSensorValues[2]) && B(irSensorValues[3]) && W(irSensorValues[4]) ){
+      error = 2;
+    }else if( W(irSensorValues[0]) && W(irSensorValues[1]) && B(irSensorValues[2]) && B(irSensorValues[3]) && W(irSensorValues[4]) ){
+      error = 1;
+    }else if( W(irSensorValues[0]) && W(irSensorValues[1]) && B(irSensorValues[2]) && W(irSensorValues[3]) && W(irSensorValues[4]) ){
+      error = 0;
+    }else if( W(irSensorValues[0]) && B(irSensorValues[1]) && B(irSensorValues[2]) && W(irSensorValues[3]) && W(irSensorValues[4]) ){
+      error = -1;
+    }else if( W(irSensorValues[0]) && B(irSensorValues[1]) && W(irSensorValues[2]) && W(irSensorValues[3]) && W(irSensorValues[4]) ){
+      error = -2;
+    }else if( B(irSensorValues[0]) && B(irSensorValues[1]) && W(irSensorValues[2]) && W(irSensorValues[3]) && W(irSensorValues[4]) ){
+      error = -3;
+    }else if( B(irSensorValues[0]) && W(irSensorValues[1]) && W(irSensorValues[2]) && W(irSensorValues[3]) && W(irSensorValues[4]) ){
+        error = -4;
+    }
+    /// check WWWWW
+    else if( W(irSensorValues[0]) && W(irSensorValues[1]) && W(irSensorValues[2]) && W(irSensorValues[3]) && W(irSensorValues[4]) ){
+        error = preError;
+    }
   if (irSensorValues[0] == LOW && irSensorValues[1] == LOW && irSensorValues[2] == LOW && irSensorValues[3] == LOW && irSensorValues[4] == LOW) {
   } else {
     Serial.print("Error Value: ");
     Serial.println(error);
-    sumError += error;
-    int speedChange = Kp * error + Kd * (error - preError);
-    int leftSpeed = baseSpeed + speedChange;
-    int rightSpeed = baseSpeed - speedChange;
-
-    // จำกัดความเร็วให้อยู่ในช่วงที่กำหนด
-    if (leftSpeed > maxSpeed) leftSpeed = maxSpeed;
-    if (rightSpeed > maxSpeed) rightSpeed = maxSpeed;
-    if (leftSpeed < -maxSpeed) leftSpeed = -maxSpeed;
-    if (rightSpeed < -maxSpeed) rightSpeed = -maxSpeed;
-
-    // แสดงค่าความเร็วทาง Serial Monitor
-    Serial.print("Left Speed: ");
-    Serial.println(leftSpeed);
-    Serial.print("Right Speed: ");
-    Serial.println(rightSpeed);
-    Serial.print("Sum Error: ");
-    Serial.println(sumError);
-
-    error = 0;
   }
-  delay(500);
+    // จำกัดความเร็วให้อยู่ในช่วงที่กำหนด
+
+   motorSpeed = Kp*error + Kd*(error - preError) + Ki*(sumError);
+   leftSpeed = baseSpeed + motorSpeed;
+   rightSpeed = baseSpeed - motorSpeed;
+   
+   if(leftSpeed > maxSpeed) leftSpeed = maxSpeed;
+   if(rightSpeed > maxSpeed) rightSpeed = maxSpeed;
+
+   if(leftSpeed < -maxSpeed) leftSpeed = -maxSpeed;
+   if(rightSpeed < -maxSpeed) rightSpeed = -maxSpeed;
+   
+   analogWrite(motorLeft,leftSpeed);
+   analogWrite(motorRight,rightSpeed);
+   delay(33); 
+
+    preError = error;
+    sumError += error;
+
+    //lcd("e=%d pe=%d|Ls=%d RS=%d|kp=%d kd=%d", error,preError, leftSpeed, rightSpeed ,Kp*error, Kd*(error - preError) );
+    
+    error = 0;
 }
